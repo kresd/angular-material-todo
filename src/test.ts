52b6a69579e97c01c385097a2925168d19fb6c10
kresd <kresnamalyuda7@gmail.com>
2021-09-14 13:31:42 +0700

    2 files modified

--- src/test.ts
+++ src/test.ts
@@ -7,29 +7,3 @@ import 'zone.js/dist/proxy';
 import 'zone.js/dist/jasmine-patch';
 import './app/app.component.spec';
 import 'zone.js/dist/zone-testing';
-import { getTestBed } from '@angular/core/testing';
-import {
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting
-} from '@angular/platform-browser-dynamic/testing';
-
-// declare const require: {
-//   context(
-//     path: string,
-//     deep?: boolean,
-//     filter?: RegExp
-//   ): {
-//     keys(): string[];
-//     <T>(id: string): T;
-//   };
-// };
-
-// // First, initialize the Angular testing environment.
-// getTestBed().initTestEnvironment(
-//   BrowserDynamicTestingModule,
-//   platformBrowserDynamicTesting()
-// );
-// // Then we find all the tests.
-// const context = require.context('./', true, /\.spec\.ts$/);
-// // And load the modules.
-// context.keys().map(context);
